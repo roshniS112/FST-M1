cd1204ce386861a343b4bb4925fe24c15497aa38
roshniS112 <154312030+roshniS112@users.noreply.github.com>
2024-02-21 12:27:25 +0530

    sql files

---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,12 @@
+REM   Script: ACTIVITY4_NEW
+REM   ACTIVITY4_NEW
+
+-- Add the grade column
+ALTER TABLE salesman ADD grade int;
+
+-- Update the values in the grade column
+UPDATE salesman SET grade=100;
+
+-- Display data
+SELECT * FROM salesman;;
+
